24300b6bf8d0f96239159493666a810fc54f35be
charles <charles.onuorah@yahoo.com>
2018-09-13 13:55:39 +0100

    fix issues with node module

---
+++ server/route/auth.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import ValidateKeys from '../services/validateKeys';
+import InputValidate from '../services/inputValidator';
+import trimSpace from '../services/trimWhiteSpace'
+// import orders from '../Order.json'
+let router = express.Router();
+
+router.get('/login', (req,res) => {
+
+})
+
+export default router;
