4f7a8cd7b25c83f52ef4a3ad3e60f248ea83260b
charles <charles.onuorah@yahoo.com>
2018-09-22 10:53:17 +0100

    Added jsdoc to DatabaseConnectio.js

--- server/DatabaseConnection.js
+++ server/DatabaseConnection.js
@@ -1,7 +1,4 @@
 import pg from 'pg';
-<<<<<<< HEAD
-import {Client} from 'pg';
-=======
 /**
  * Creates a connection to database.
  *
@@ -10,21 +7,14 @@ import {Client} from 'pg';
  * 
  *
  */
->>>>>>> develop-challenge3
 require('dotenv').config();
 let pool;
 if(process.env.NODE_ENV ==='DEVELOPMENT'){
      
     const config = {
-<<<<<<< HEAD
-        user: process.env.DB_USER,
-        database: process.env.LOCAL_DB, 
-        password: process.env.DB_PASSWORD, 
-=======
         user: DB_USER,
         database: LOCAL_DB, 
         password: DB_PASSWORD, 
->>>>>>> develop-challenge3
         port: 5432, 
         max: 20, // max number of connection can be open to database
         idleTimeoutMillis: 50000,
@@ -34,14 +24,8 @@ if(process.env.NODE_ENV ==='DEVELOPMENT'){
 }
 else if(process.env.NODE_ENV === 'TEST'){
      pool = new pg.Pool({
-<<<<<<< HEAD
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-      });
-=======
         connectionString:DATABASE_URL, ssl:true
     });
->>>>>>> develop-challenge3
  }
 else{
     pool = new pg.Pool({
