58fc7e65220d84d04fa858943efbfd046b20a013
charles <charles.onuorah@yahoo.com>
2018-09-25 14:43:38 +0100

    resolved merge conflicts

--- server/test/test.js
+++ server/test/test.js
@@ -292,4 +292,50 @@ describe('Test all api end points', function(){
             })
         })
     })
+    describe('it should sign up a user',() => {
+        this.timeout(40000);
+        it('response should be an object', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res).to.be.an('object');
+                done();
+            })
+        })
+        it('response to have property message', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res.body).to.have.property('message');
+                done();
+            })
+        })
+        it('response should have a status of 201',(done)=>{
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                
+                expect(res).to.have.status(201);
+                done();
+            })
+        })
+        it('response to have property token', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res.body).to.have.property('token');
+                done();
+            })
+        })
+        it('response to have property roleid', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res.body).to.have.property('roleid');
+                done();
+            })
+        })
+        it('response to have property rolename', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res.body).to.have.property('rolename');
+                done();
+            })
+        })
+        it('response to have property useremail', function(done){
+            chai.request(app).post('/api/v1/auth/signup').type('form').send(createuser).end(function(err,res){
+                expect(res.body).to.have.property('useremail');
+                done();
+            })
+        })
+    })
 })
